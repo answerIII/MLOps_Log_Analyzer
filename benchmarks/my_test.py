2981bdda9e27bff73d4ef79525c7b73cb8fe3ad3
Dmitriy Volf <d.volf@yadro.com>
2022-04-10 00:46:12 +0300

    first commit in new branch

--- benchmarks/my_test.py
+++ benchmarks/my_test.py
@@ -133,44 +133,19 @@ x_test = USE_embed(x_test)
 x_train = np.array(x_train)
 x_test = np.array(x_test)
 
-10/0
+loglizer = False
 
-# TF-IDF from loglizer
-# feature_extractor = my_tf_idf.FeatureExtractor()
-# x_train = feature_extractor.fit_transform(x_train, term_weighting='tf-idf')
-# x_test = feature_extractor.transform(x_test)
-# [('SGDoneSVM', {'precision': 0.11463452195948311, 'recall': 0.9725620620026132, 'f1': 0.20509480750444609}),
-# ('IsolationForest', {'precision': 0.25347675269706904, 'recall': 0.9460743556241834, 'f1': 0.39982932583705644}),
-# ('OneSVM', {'precision': 0.3965709304015111, 'recall': 0.9725620620026132, 'f1': 0.5634074176013212}),
-# ('RandomForest', {'precision': 0.9991374353076481, 'recall': 0.41275685948449936, 'f1': 0.5841808859376314}),
-# ('CatBoost', {'precision': 0.9771108850457783, 'recall': 0.9126974700083146, 'f1': 0.943806423877664}),
-# ('LinearSVM', {'precision': 0.9464392222354766, 'recall': 0.9423922081007245, 'f1': 0.9444113795976671}),
-# ('SVM', {'precision': 0.9923366986984552, 'recall': 0.9689986934315239, 'f1': 0.9805288461538462}),
-# ('XGBoost', {'precision': 0.9971537001897534, 'recall': 0.998693431523934, 'f1': 0.9979229719304492})]
-
-# TF-IDF from sklearn
-vectorizer = TfidfVectorizer()
-x_train = vectorizer.fit_transform(x_train).toarray()
-vectorizer.get_feature_names_out()
-x_test = vectorizer.transform(x_test).toarray()
-# [('OneSVM', {'precision': 0.035771606974034253, 'recall': 0.9662667775270222, 'f1': 0.06898920427080064}),
-# ('IsolationForest', {'precision': 0.1646695244741302, 'recall': 0.9809953676208576, 'f1': 0.28200225355959985}),
-# ('SGDoneSVM', {'precision': 0.3920792079207921, 'recall': 0.9642475353367383, 'f1': 0.5574783683559951}),
-# ('RandomForest', {'precision': 0.9991286668602962, 'recall': 0.40859959615156194, 'f1': 0.5800033721126285}),
-# ('CatBoost', {'precision': 0.894896648370898, 'recall': 0.9102031120085521, 'f1': 0.9024849841008126}),
-# ('LinearSVM', {'precision': 0.9602474510253179, 'recall': 0.9956051787623234, 'f1': 0.9776067179846046}),
-# ('SVM', {'precision': 0.9931222577967509, 'recall': 0.9947737260957359, 'f1': 0.9939473059577499}),
-# ('XGBoost', {'precision': 0.9972713251868549, 'recall': 0.9984558736191946, 'f1': 0.997863247863248})])
-
-# TF-IDF from sklearn + scaler = StandardScaler()
-# [('OneSVM', {'precision': 0.11582379484921858, 'recall': 1.0, 'f1': 0.20760230313043262}),
-# ('IsolationForest', {'precision': 0.17271869358279993, 'recall': 1.0, 'f1': 0.294561167188566}),
-# ('RandomForest', {'precision': 0.9991291727140784, 'recall': 0.4088371540563012, 'f1': 0.5802427511800404}),
-# ('SGDoneSVM', {'precision': 0.4386038030737171, 'recall': 1.0, 'f1': 0.6097631636126603}),
-# ('CatBoost', {'precision': 0.8887858054041516, 'recall': 0.9103218909609218, 'f1': 0.899424950123225}),
-# ('LinearSVM', {'precision': 0.9605866177818515, 'recall': 0.9958427366670626, 'f1': 0.9778970082230127}),
-# ('SVM', {'precision': 0.9943208707998107, 'recall': 0.9982183157144554, 'f1': 0.9962657815185821}),
-# ('XGBoost', {'precision': 0.9972713251868549, 'recall': 0.9984558736191946, 'f1': 0.997863247863248})]
+if loglizer:
+    # TF - IDF from loglizer
+    feature_extractor = my_tf_idf.FeatureExtractor()
+    x_train = feature_extractor.fit_transform(x_train, term_weighting='tf-idf')
+    x_test = feature_extractor.transform(x_test)
+else:
+    # TF-IDF from sklearn
+    vectorizer = TfidfVectorizer()
+    x_train = vectorizer.fit_transform(x_train).toarray()
+    vectorizer.get_feature_names_out()
+    x_test = vectorizer.transform(x_test).toarray()
 
 # scale
 scaler = StandardScaler()
